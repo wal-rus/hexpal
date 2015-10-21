689e4dd170786a66622730ce21c03d768caa51f2
Walter Gray <wgray@leapmotion.com>
2015-10-21 00:33:40 -0700

    Added basic canvas rendering green using glsl shaders

---
+++ web/scripts/render.js
@@ -0,0 +1,31 @@
+var canvas = document.getElementById("canvas")
+var gl = canvas.getContext("webgl");
+
+// setup a GLSL program
+var vertexShader = createShaderFromScriptElement(gl, "2d-vertex-shader");
+var fragmentShader = createShaderFromScriptElement(gl, "2d-fragment-shader");
+var program = createProgram(gl, [vertexShader, fragmentShader]);
+gl.useProgram(program);
+
+// look up where the vertex data needs to go.
+var positionLocation = gl.getAttribLocation(program, "a_position");
+
+// Create a buffer and put a single clipspace rectangle in
+// it (2 triangles)
+var buffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+gl.bufferData(
+    gl.ARRAY_BUFFER, 
+    new Float32Array([
+        -1.0, -1.0, 
+         1.0, -1.0, 
+        -1.0,  1.0, 
+        -1.0,  1.0, 
+         1.0, -1.0, 
+         1.0,  1.0]), 
+    gl.STATIC_DRAW);
+gl.enableVertexAttribArray(positionLocation);
+gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, 0);
+
+// draw
+gl.drawArrays(gl.TRIANGLES, 0, 6);
